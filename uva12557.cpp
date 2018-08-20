59e8338e260115ddc3c33ad222945e5ef6ed767c
Rui-Jie Fang <ruijie.fang@temple.edu>
2018-08-20 02:47:48 -0400

    add problems

---
+++ uva12557.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <memory.h>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+int main()
+{ int T;
+  cin >> T;
+  while (T--) {
+    string x,y;
+    cin >> x >> y;
+
+  }
+  return 0;
+}
