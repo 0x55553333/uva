ea0068823fe6947e41ea00b5496240556dca63f6
Rui-Jie Fang <rui.jie.fang@gmail.com>
2018-10-24 14:14:06 -0400

    Union by rank

---
+++ repo/kruskal.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <memory.h>
+using namespace std;
+
+
+int main()
+{
+  return 0;
+}
--- repo/ufd.cpp
+++ repo/ufd.cpp
@@ -5,6 +5,40 @@
 using namespace std;
 // union-find-disjoint set
 
+struct disjoint_set
+{
+  vector<long> parent;
+  vector<int> rank;
+  long find(long i)
+  {
+    if (parent[i] == i) return i;
+    else return parent[i] = find(parent[i]);
+  }
+
+  bool eq(long i, long j)
+  {
+    return find(i) == find(j);
+  }
+
+  void union(long i, long j)
+  {
+    long x = find(i), y = find(j);
+    if (rank[x] > rank[y]) p[y] = x;
+    else {
+      p[x] = y;
+      if (rank[x] == rank[y]) ++rank[y];
+    }
+  }
+
+  disjoint_set(long n)
+  {
+    this->rank.assign(n, 0);
+    this->parent.assign(n, 0);
+    for(long i = 0; i < n; ++i)
+      this->parent[i] = i;
+  }
+};
+
 int find(int *p, int x)
 {
 	return p[x] != x ? p[x] = find(p, p[x]) : x;
