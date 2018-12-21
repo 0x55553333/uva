3118730f61ddfddcb3b0d82f93109accf0cdaf3a
Rui-Jie Fang <rui.jie.fang@gmail.com>
2018-12-21 16:33:06 -0500

    Add comments for UVa 1437 - String Painter

--- uva1437.cpp
+++ uva1437.cpp
@@ -9,16 +9,18 @@ using namespace std;
 char A[200], B[200];
 int N, mc[200][200], mopt[200], vc[200][200], vopt[200];
 
+// Compute the cost of painting a completely mismatched 
+// string to the target string B
 int cost(int i, int j);
 int _cost(int i, int j)
 {
   int ans = 0xfffff;
-  if (i == j) return 1;
-  else if (B[i] != B[j]) 
+  if (i == j) return 1; // Basecase: Charge for a single painting operation
+  else if (B[i] != B[j]) // Divide into two sub-intervals
     for(int k = i; k < j; ++k) {
       ans = min(ans, cost(i, k) + cost(k+1, j));
     } 
-  else
+  else // If two ends are the same, only charge a single end
     ans = cost(i, j-1); // charging the rightmost piece = charging the leftmost piece
 //    ans = max(cost(i+1, j), cost(i, j-1));
 //    ans = cost(i+1, j);
@@ -34,12 +36,13 @@ int cost(int i, int j)
   }
 }
 
+// Compute the optimal cost of painting string A to target string B
 int opt(int n);
 int _opt(int n)
 {
-  if (n == 0) 
+  if (n == 0) // basecase: cost=1 if A[0]!=B[0]
     return A[0] != B[0];
-  else {
+  else { // Find an optimal index towards the left of the string
     int ans = cost(0, n);
     for(int k = 0; k < n; ++k)
       ans = min(ans, opt(k) + cost(k+1, n));
