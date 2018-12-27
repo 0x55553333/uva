fbfb4dc0232e9b2387c215136baedbadc9dbeb9b
Rui-Jie Fang <rui.jie.fang@gmail.com>
2018-12-27 14:28:53 -0500

    Compress code size of UFDs for Kruskal

--- repo/ufd.cpp
+++ repo/ufd.cpp
@@ -6,38 +6,19 @@ using namespace std;
 // union-find-disjoint set
 
 struct disjoint_set
-{
-  vector<long> parent;
-  vector<int> rank;
-  long find(long i)
-  {
-    if (parent[i] == i) return i;
-    else return parent[i] = find(parent[i]);
-  }
-
-  bool eq(long i, long j)
-  {
-    return find(i) == find(j);
-  }
-
-  void merge(long i, long j)
-  {
-    long x = find(i), y = find(j);
+{ vector<long> parent; vector<int> rank;
+  long find(long i) // Find a parent of a set
+  { if (parent[i] == i) return i;
+    else return parent[i] = find(parent[i]);}
+  bool eq(long i, long j){return find(i) == find(j);} // Test two parents
+  void merge(long i, long j) // Merge two sets
+  { long x = find(i), y = find(j);
     if (rank[x] > rank[y]) parent[y] = x;
-    else {
-      parent[x] = y;
-      if (rank[x] == rank[y]) ++rank[y];
-    }
-  }
-
-  disjoint_set(long n)
-  {
-    this->rank.assign(n, 0);
-    this->parent.assign(n, 0);
-    for(long i = 0; i < n; ++i)
-      this->parent[i] = i;
-  }
-};
+    else { parent[x] = y;
+      if (rank[x] == rank[y]) ++rank[y]; }}
+  disjoint_set(long n) // n: size of set
+  { this->rank.assign(n, 0); this->parent.assign(n, 0);
+    for(long i = 0; i < n; ++i) this->parent[i] = i; }};
 
 int main()
 {
--- repo/ufd_generic.cpp
+++ repo/ufd_generic.cpp
@@ -7,59 +7,26 @@ using namespace std;
 // for generic content
 template <typename T>
 struct disjoint_set
-{
-  vector<T> data;
-  vector<long> parent;
-  vector<int> rank;
+{ vector<T> data;vector<long> parent;vector<int> rank;
   T identity;
   long find(long i)
-  {
-    if (parent[i] == i) return i;
-    else return parent[i] = find(parent[i]);
-  }
-
+  { if (parent[i] == i) return i; else return parent[i] = find(parent[i]); }
   T find_parent(long i)
-  {
-    long idx = find(i);
-    return data[idx];
-  }
-
+  { long idx = find(i); return data[idx]; }
   T at(long i)
-  {
-    return data[i];
-  }
-
+  { return data[i]; }
   bool eq(long i, long j)
-  {
-    return data[find(i)] == data[find(j)];
-  }
-
+  { return data[find(i)] == data[find(j)]; }
   void merge(long i, long j)
-  {
-    long x = find(i), y = find(j);
+  { long x = find(i), y = find(j);
     if (rank[x] > rank[y]) parent[y] = x;
-    else {
-      parent[x] = y;
-      if (rank[x] == rank[y]) ++rank[y];
-    }
-  }
-
+    else { parent[x] = y; if (rank[x] == rank[y]) ++rank[y]; } }
   void assign(long i, T& elem)
-  {
-    long idx = find(i);
-    this->data[idx] = elem;
-  }
-
+  { long idx = find(i); this->data[idx] = elem; }
   disjoint_set(long n, T& identity)
-  {
-    this->identity = identity;
-    this->rank.assign(n, 0);
-    this->parent.assign(n, 0);
-    this->data.assign(n, identity);
-    for(long i = 0; i < n; ++i)
-      this->parent[i] = i;
-  }
-};
+  { this->identity = identity; this->rank.assign(n, 0);
+    this->parent.assign(n, 0); this->data.assign(n, identity);
+    for(long i = 0; i < n; ++i) this->parent[i] = i; } };
 
 int main()
 {
