262a25cab3392f8bad6cdc4b200e48dedc2ca643
Rui-Jie Fang <rui.jie.fang@gmail.com>
2018-12-27 21:14:46 -0500

    Add multi-source BFS for grid graph

---
+++ repo/bfs_grid.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <utility>
+#include <vector>
+#include <memory.h>
+#include <queue>
+#define MAXN 100
+using namespace std;
+int N;
+char grid[MAXN][MAXN];
+int dirs[][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+int dist[MAXN][MAXN];
+int parent[MAXN][MAXN][2];
+int bfs(char s, char t)
+{
+  memset(dist, 0, sizeof(dist));
+  memset(parent, 0, sizeof(parent));
+  queue< pair<int,int> > Q;
+  for(int i = 0; i < N; ++i)
+    for(int j = 0; j < N; ++j) {
+      dist[i][j] = 0xfffff;
+      parent[i][j][0] = parent[i][j][1] = -1;
+      if (grid[i][j] == s) {
+        dist[i][j] = 0;
+        Q.push(make_pair(i, j));
+      }
+    }
+  int minCost=0xfffff;
+  while (!Q.empty()) {
+    pair<int,int> u = Q.front(); Q.pop();
+    if (grid[u.first][u.second]==t) {
+      minCost = min(minCost, dist[u.first][u.second]);
+      continue;
+    }
+    for(int i = 0; i < 4; ++i) { // all 4 directions
+      int ii = u.first + dirs[i][0], jj = u.second + dirs[i][1];
+      if (grid[ii][jj] == '.' || grid[ii][jj] == t) 
+        if (dist[u.first][u.second] + 1 <= dist[ii][jj]) {
+          parent[ii][jj][0] = u.first; parent[ii][jj][1] = u.second;
+          dist[ii][jj] = dist[u.first][u.second] + 1;
+          Q.push(make_pair(ii,jj));
+        }
+    }
+  }
+  return minCost;
+}
+
+int main()
+{ char s, t;
+  cin >> N;
+  for(int i =0; i < N; ++i)
+    scanf("%s", grid[i]);
+  while (cin >> s >> t) {
+    if (s == '$' || t == '$') break;
+    int cost = bfs(s, t);
+    printf("%d\n", cost);
+  }
+  return 0;
+}
+
